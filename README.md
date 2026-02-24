f37266b77a177958e6258aabf40ad5f8b041b20a
Zuri McKee <zurimckee95@gmail.com>
2026-02-23 21:31:47 -0500

    api key set up will finish making it pretty later

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 💬 Chatbot template
+# 💬 AggieBot
 
-A simple Streamlit app that shows how to build a chatbot using OpenAI's GPT-3.5.
+A&T AI ChatBot
 
 [![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://chatbot-template.streamlit.app/)
 
@@ -16,4 +16,10 @@ A simple Streamlit app that shows how to build a chatbot using OpenAI's GPT-3.5.
 
    ```
    $ streamlit run streamlit_app.py
+   ```
+3. If you don't have a Gemini API key
+
+   ```
+   This is a chatbot that uses Gemini 3 Fast model to generate responses. "
+        "To use this app, you need to provide an Gemini API key, which you can get [here](https://makersuite.google.com/app/apikey)."
    ```
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,122 @@
+
 import streamlit as st
-from openai import OpenAI
+from google import genai as genai
+from google.genai import types
+
+
 
 # Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+def config_page():
+    st.set_page_config(
+        page_title="AggieBot",
+        page_icon = "assets/favicon.ico",
+        layout= "centered",
+    )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+    with st.container(border=True, width = "stretch"):
+        st.space(5)
+        st.header("Hey I'm AggieBot! 💬 ", divider= False, text_alignment="center")
+        st.space(1)
+        st.write("Let's get started! Ask me anything about North Carolina A&T and I'll do my best to answer your question!")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+def buttons():
+    st.button(
+        label = "When does the financial aid office close?",
+        type = "primary",
+        disabled = False,
+        width = "content",
+        #on_click = lambda: st.session_state.update(prompt = "When does the financial aid office close?")
+    )
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+def config_api_key():
+    """configures api key and basic text interface"""
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # Ask user for their Gemini API key via `st.text_input`.
+    # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+    # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+    gemini_api_key = st.secrets["GEMINI_API_KEY"] if "GEMINI_API_KEY" in st.secrets else st.text_input("Enter your Gemini API key", type="password")
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
+    if gemini_api_key:
+
+        #initializes client
+        client = genai.Client(api_key = gemini_api_key)
+        #makes sure that config is stored in session state so that it doesn't have to be recreated on every rerun.
+        st.session_state.aggie_config = types.GenerateContentConfig(
+            system_instruction = "You are AggieBot, an AI assistant meant to help students, " \
+            "faculty, and staff of North Carolina Agricultural and Technical State University " \
+            "with any questions that they may have about the university and its resources. " \
+            "You should answer questions to the best of your ability, but if you don't know the answer, " \
+            "say you don't know and suggest that the user contact the university directly for more information.",
+            temperature=0.7,
+            max_output_tokens = 1500,
+            #in case of 503 errors retry up to 5 times with exponential backoff
+            http_options = types.HttpOptions(
+                retry_options = types.HttpRetryOptions(
+                    attempts = 5,
+                    http_status_codes = [503]
+                )
+            ),
+            safety_settings =[
+                types.SafetySetting(
+                    category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                    threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+                )
             ],
-            stream=True,
         )
+            
+        
+        # Create a session state variable to store the chat messages. This ensures that the
+        # messages persist across reruns.
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        # Display the existing chat messages via `st.chat_message`.
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # Create a chat input field to allow the user to enter a message. This will display
+        # automatically at the bottom of the page.
+        if prompt := st.chat_input("How can I help?"):
+
+            # Store and display the current prompt.
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            # Generate a response using the Gemini API.
+
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+
+                with st.spinner("Thinking..."):
+        
+                    response = client.models.generate_content_stream(
+                        model = "gemini-3-flash-preview",
+                        contents = prompt,
+                        config = st.session_state.aggie_config
+                    )
+    
+                for chunk in response:
+                    full_response += chunk.text
+                    message_placeholder.markdown(full_response)
+                
+                st.session_state.messages.append({"role" : "assistant", "content": full_response})
+
+    else:
+        st.warning("Please enter your Gemini API key to use the chatbot. You can get an API key from the [Google Cloud Console](https://console.cloud.google.com/apis/credentials).")
+
+
+
+
+
+
+if __name__ == "__main__":
+    with st.container(border=True, width="stretch"):
+        config_page()
+        buttons()
+        #make_it_pretty()
+        config_api_key()
